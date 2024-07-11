84836cb9406a580c9a63cd2d0802556f03920dfa
Santosh Shah <santoshofficial679@gmail.com>
2024-07-12 03:56:01 +0545

    I am ready now

---
+++ src/Java_Journey_July_12_2024/Main.java
@@ -0,0 +1,7 @@
+package Java_Journey_July_12_2024;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hariom Shah");
+    }
+}
---
+++ src/Java_Journey_July_12_2024/OOps_Concepts/Main.java
@@ -0,0 +1,7 @@
+package Java_Journey_July_12_2024.OOps_Concepts;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hariom Shah");
+    }
+}
